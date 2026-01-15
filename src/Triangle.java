bde2ecd21a02145fafb7d26ddc803be592f4df71
govind <govind.g.coder@gmail.com>
2026-01-15 11:57:05 +0530

    initial commit

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,13 @@
 public class Triangle {
-    //Add your programs here
+    public static void main(String[] args){
+        for(int i=0;i<3;i++){
+            // for 0 -> 1 star, 1-> 3 star, 2-> 5 star
+            int j=i*2+1;
+            while(j>1){
+                System.out.print("* ");
+                j--;
+            }
+            System.out.print("*\n");
+        }
+    }
 }
